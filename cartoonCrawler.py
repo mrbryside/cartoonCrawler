81f01ef98085e67b499d99c23102cd1d00ba2f26
vagrant <vagrant@homestead>
2018-12-22 07:24:26 +0000

    edit mysql

--- cartoonCrawler.py
+++ cartoonCrawler.py
@@ -7,11 +7,47 @@ import re
 import os
 import requests
 from bs4 import BeautifulSoup
-import selenium.webdriver.support.ui as ui
+import mysql.connector
+import datetime
+import pytz
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="homestead",
+  passwd="secret",
+  database="cartoonAPI"
+)
+
+cartoonID = 0
+mycursor = mydb.cursor()
+now = datetime.datetime.now(pytz.timezone('Asia/Bangkok'))
 
 print("---------- Cartoon Crawler Start !!  ----------")
-if not os.path.exists('/var/www/everygrams.ddns.net/cartoonAPI/public/onepiece'):
-    os.system('sudo mkdir /var/www/everygrams.ddns.net/cartoonAPI/public/onepiece')   
+# if not os.path.exists('/var/www/everygrams.ddns.net/cartoonAPI/public/onepiece'):
+#     os.system('sudo mkdir /var/www/everygrams.ddns.net/cartoonAPI/public/onepiece')   
+if not os.path.exists('/home/vagrant/sites/cartoonAPI/public/onepiece'):
+    os.system('sudo mkdir /home/vagrant/sites/cartoonAPI/public/onepiece') 
+mycursor.execute("SELECT * FROM cartoons") 
+myresult = mycursor.fetchall()
+found = 0
+foundID = 0
+for x in myresult:
+    if x[1] == "Onepiece":      
+        found = 1
+        foundID = x[0]
+
+if found == 0 :
+    sql = "INSERT INTO cartoons (cartoonName,created_at,updated_at) VALUES (%s,%s,%s)"
+    val = ("Onepiece",now,now,)
+    mycursor.execute(sql, val)
+    
+    mydb.commit()
+
+    print(mycursor.rowcount, "update to databse success!")
+    cartoonID = mycursor.lastrowid
+elif found == 1:
+    cartoonID = foundID
+    
 round = 1
 while round <= 3 :
     
@@ -44,19 +80,29 @@ while round <= 3 :
         part_name = re.search('\+\d+\-',part)
         part_name = re.sub("\D", "", part_name.group(0))
         
-        if not os.path.exists('/var/www/everygrams.ddns.net/cartoonAPI/public/onepiece/'+part_name):
-            os.system('sudo mkdir /var/www/everygrams.ddns.net/cartoonAPI/public/onepiece/'+part_name)   
+        # if not os.path.exists('/var/www/everygrams.ddns.net/cartoonAPI/public/onepiece/'+part_name):
+        #     os.system('sudo mkdir /var/www/everygrams.ddns.net/cartoonAPI/public/onepiece/'+part_name)  
+        if not os.path.exists('/home/vagrant/sites/cartoonAPI/public/onepiece/'+part_name):
+            os.system('sudo mkdir /home/vagrant/sites/cartoonAPI/public/onepiece/'+part_name) 
         else:
             print('downloaded all break!')
-            break
+            continue
         for tag in soup.find_all('img'):
             print('save image :'+str(count_image))
-            f = open('/var/www/everygrams.ddns.net/cartoonAPI/public/onepiece/'+part_name+'/'+str(count_image), 'wb')
+            # f = open('/var/www/everygrams.ddns.net/cartoonAPI/public/onepiece/'+part_name+'/'+str(count_image), 'wb')
+            f = open('/home/vagrant/sites/cartoonAPI/public/onepiece/'+part_name+'/'+str(count_image), 'wb')
             f.write(urllib2.urlopen(tag['src']).read())
             f.close()
 
             count_image +=1
         print('part '+str(count_2)+' finished !')
+        sql = "INSERT INTO cartoonlists (chapterID,cartoonPage,cartoonID,created_at,updated_at) VALUES (%s,%s,%s,%s,%s)"
+        val = (part_name,count_image,cartoonID,now,now,)
+        mycursor.execute(sql, val)
+
+        mydb.commit()
+
+        print(mycursor.rowcount, "update to database success!!")
         count_2 +=1
     round+=1
 
